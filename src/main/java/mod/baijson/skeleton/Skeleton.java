a3d6adaaf5f6d2ea52d47a4fe65fef0ae9261eeb
Baijson <dev@baijbot.tv>
2017-04-30 01:30:59 +0200

    version

--- src/main/java/mod/baijson/skeleton/Skeleton.java
+++ src/main/java/mod/baijson/skeleton/Skeleton.java
@@ -19,7 +19,7 @@ public class Skeleton {
     public static final String MODID = "skeleton";
     public static final String MODNM = "Skeleton";
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     public static final String PROXY_COMMON = "mod.baijson." + MODID + ".common.CommonProxy";
     public static final String PROXY_CLIENT = "mod.baijson." + MODID + ".client.ClientProxy";
