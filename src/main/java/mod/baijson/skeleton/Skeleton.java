51220020cd031df69138fcca5101cee8dfa4591e
Baijson <dev@baijbot.tv>
2017-05-13 00:24:06 +0200

    bumped version

--- src/main/java/mod/baijson/skeleton/Skeleton.java
+++ src/main/java/mod/baijson/skeleton/Skeleton.java
@@ -19,7 +19,7 @@ public class Skeleton {
     public static final String MODID = "skeleton";
     public static final String MODNM = "Skeleton";
 
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     public static final String PROXY_COMMON = "mod.baijson." + MODID + ".common.CommonProxy";
     public static final String PROXY_CLIENT = "mod.baijson." + MODID + ".client.ClientProxy";
