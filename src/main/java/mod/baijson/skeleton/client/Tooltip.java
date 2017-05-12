de8b2df84b881a9b9cdcca4bdfb2c65b513b2314
Baijson <dev@baijbot.tv>
2017-05-13 00:23:34 +0200

    fixed crash on shift click

--- src/main/java/mod/baijson/skeleton/client/Tooltip.java
+++ src/main/java/mod/baijson/skeleton/client/Tooltip.java
@@ -11,13 +11,13 @@ import java.util.List;
 /**
  * Created by Baijson.
  */
-@SideOnly(Side.CLIENT)
 public class Tooltip {
 
     /**
      * @param tooltip
      * @param runnable
      */
+    @SideOnly(Side.CLIENT)
     static public void construct(List<String> tooltip, Runnable runnable) {
         if (GuiScreen.isShiftKeyDown()) {
             runnable.run();
@@ -31,24 +31,37 @@ public class Tooltip {
      * @param message
      * @param parameters
      */
+    @SideOnly(Side.CLIENT)
     static public void insert(List<String> tooltip, String message, @Nullable Object... parameters) {
-        message = local(message).replace("&", "\u00a7");
-        if (parameters != null) {
-            Object[] values = new String[parameters.length];
-            for (int i = 0; i < parameters.length; i++) {
-                values[i] = local(parameters[i].toString()).replaceAll("&", "\u00a7");
-            }
-            if (values.length > 0) {
-                message = String.format(message, values);
-            }
+
+        message = local(message).replaceAll("&", "\u00a7");
+
+        Object[] values = new String[parameters.length];
+        for (int i = 0; i < parameters.length; i++) {
+            values[i] = local(parameters[i].toString()).replaceAll("&", "\u00a7");
         }
+        if (values != null && values.length > 0) {
+            message = String.format(message, values);
+        }
+
         tooltip.add(message);
     }
 
     /**
+     * @param string
      * @return
      */
-    static public String local(String string, @Nullable Object... parameters) {
+    @SideOnly(Side.CLIENT)
+    public static String local(String string) {
+        return I18n.format(string);
+    }
+
+    /**
+     * @param string
+     * @return
+     */
+    @SideOnly(Side.CLIENT)
+    public static String local(String string, Object... parameters) {
         return I18n.format(string, parameters);
     }
 }
